6f5eda109c89b0d3fc53338e7f7a8592adb8560d
Emma Fodor <fodor_ema@yahoo.com>
2022-11-22 22:20:36 +0200

    Add project files.

---
+++ AboutPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Fodor_Emanuela_Lab7;
+
+public partial class AboutPage1 : ContentPage
+{
+	public AboutPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Fodor_Emanuela_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
